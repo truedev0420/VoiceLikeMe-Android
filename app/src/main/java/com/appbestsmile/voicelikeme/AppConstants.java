fb77326dfbe6871ff0d79356bdbab47536f52f8c
julian-medve <julian.medve.superdev@gmail.com>
2020-02-12 23:19:04 +1000

    no message

---
+++ app/src/main/java/com/appbestsmile/voicelikeme/AppConstants.java
@@ -0,0 +1,18 @@
+package com.appbestsmile.voicelikeme;
+
+import android.content.Context;
+
+import com.walid.speex.Speex;
+
+public class AppConstants {
+  public static final String ACTION_PAUSE = "com.appbestsmile.voicelikeme.PAUSE";
+  public static final String ACTION_RESUME = "com.appbestsmile.voicelikeme.RESUME";
+  public static final String ACTION_STOP = "com.appbestsmile.voicelikeme.STOP";
+  public static final String ACTION_IN_SERVICE = "com.appbestsmile.voicelikeme.ACTION_IN_SERVICE";
+
+//  public static Context applicationContext;
+
+  public static int PLAY_COUNT = 5;
+
+  public static Speex speex;
+}
