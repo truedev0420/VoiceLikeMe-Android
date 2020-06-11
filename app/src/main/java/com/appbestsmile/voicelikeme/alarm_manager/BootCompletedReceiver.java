50b7ca73e261d593a2384007f867f4c2480d372c
julian-medve <julian.medve.superdev@gmail.com>
2020-06-12 04:14:43 +0900

    Run watch schedule thread on boot completed receiver.

--- app/src/main/java/com/appbestsmile/voicelikeme/alarm_manager/BootCompletedReceiver.java
+++ app/src/main/java/com/appbestsmile/voicelikeme/alarm_manager/BootCompletedReceiver.java
@@ -1,28 +1,12 @@
 package com.appbestsmile.voicelikeme.alarm_manager;
 
-import android.annotation.SuppressLint;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Handler;
-import android.util.Log;
-
-import com.appbestsmile.voicelikeme.db.ScheduleItem;
-import com.appbestsmile.voicelikeme.db.ScheduleItemDataSource;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.inject.Inject;
-
-import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.schedulers.Schedulers;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
 
     private static final String TAG = BootCompletedReceiver.class.getSimpleName();
-    @Inject
-    public ScheduleItemDataSource scheduleItemDataSource;
 
     @Override
     public void onReceive(Context context, Intent intent) {
@@ -34,21 +18,8 @@ public class BootCompletedReceiver extends BroadcastReceiver {
             if(intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED) ||
                     intent.getAction().equals(Intent.ACTION_USER_PRESENT))
             {
-                /*Handler handler = new Handler();
-
-                Thread thread = new Thread() {
-                    @SuppressLint("CheckResult")
-                    @Override
-                    public void run() {
-
-
-                    }
-                };
-
-                thread.start();*/
-
-//                WatchScheduleThread scheduleThread = new WatchScheduleThread();
-//                scheduleThread.start();
+                WatchScheduleThread scheduleThread = new WatchScheduleThread(context);
+                scheduleThread.start();
             }
         }
     }
